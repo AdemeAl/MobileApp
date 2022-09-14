0e324344105fa2e42259d570506ace042f98d9ed
Ademe <ademealaoui@gmail.com>
2022-09-14 17:09:09 +0200

    project

---
+++ toogle.js
@@ -0,0 +1,13 @@
+
+
+
+
+const previous = document.querySelector('.video1');
+
+function toogleVideo() {
+    const trailer = document.querySelector('.video1');
+    trailer.classList.add('active')
+};
+
+
+
