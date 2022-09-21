6bda78cd5f6a9ef6e42983ac32ed296c64b06e9f
Ademe <ademealaoui@gmail.com>
2022-09-21 15:06:10 +0200

    project

--- sw.js
+++ sw.js
@@ -57,12 +57,23 @@ const CACHE_NAME = 'offline';
 // Customize this with a different URL if needed.
 const OFFLINE_URL = 'offline.html';
 
+const CACHE_FILE = '/style.css';
+
 self.addEventListener('install', (event) => {
     event.waitUntil((async () => {
         const cache = await caches.open(CACHE_NAME);
         // Setting {cache: 'reload'} in the new request will ensure that the response
         // isn't fulfilled from the HTTP cache; i.e., it will be from the network.
         await cache.add(new Request(OFFLINE_URL, { cache: 'reload' }));
+    })());
+});
+
+self.addEventListener('install', (event) => {
+    event.waitUntil((async () => {
+        const cache = await caches.open(CACHE_NAME);
+        // Setting {cache: 'reload'} in the new request will ensure that the response
+        // isn't fulfilled from the HTTP cache; i.e., it will be from the network.
+        await cache.add(new Request(CACHE_FILE, { cache: 'reload' }));
     })());
 });
 
